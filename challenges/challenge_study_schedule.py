95afe73f3c5643dc10cad63f67b1dbf4ae4012bc
jonathan <jonathan.wiz@outlook.com>
2023-03-23 11:34:47 -0300

    Req 1 - Get the number of students, studying at the same time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    for i in permanence_period:
+        if type(i[0]) != int or type(i[1]) != int or type(target_time) != int:
+            return None
+        if target_time in range(i[0], i[1] + 1):
+            counter += 1
+    return counter
